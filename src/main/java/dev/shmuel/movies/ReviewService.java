88aafb66b5b831dbeec2c456d4523b11c08894e0
Moshiach770 <109598704+samWW2@users.noreply.github.com>
2023-09-03 20:27:16 +0300

    final commit

--- src/main/java/dev/shmuel/movies/ReviewService.java
+++ src/main/java/dev/shmuel/movies/ReviewService.java
@@ -11,14 +11,15 @@ public class ReviewService {
 
     @Autowired
     private ReviewRepository repository;
+    @Autowired
     private MongoTemplate mongoTemplate;
 
-    public Review createReview(String reviewBody, String imdbId){
+    public Review createReview(String reviewBody, String imdbId) {
         Review review = repository.insert(new Review(reviewBody));
 
         mongoTemplate.update(Movie.class)
                 .matching(Criteria.where("imdbId").is(imdbId))
-                .apply(new Update().push("reviewIds").value(review))
+                .apply(new Update().push("reviews").value(review))
                 .first();
 
         return review;
